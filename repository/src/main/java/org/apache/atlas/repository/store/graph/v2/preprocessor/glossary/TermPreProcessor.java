859cc61e9cb660eeedfb65c2485deed3f515f403
RexterR <sumandas.workplace@gmail.com>
2022-04-18 12:49:32 +0530

    Added Term Aggregation by name

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/preprocessor/glossary/TermPreProcessor.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/preprocessor/glossary/TermPreProcessor.java
@@ -95,7 +95,6 @@ public class TermPreProcessor implements PreProcessor {
         }
 
         entity.setAttribute(QUALIFIED_NAME, createQualifiedName());
-        entity.setAttribute(NAME, termName);
         AtlasAuthorizationUtils.verifyAccess(new AtlasEntityAccessRequest(typeRegistry, AtlasPrivilege.ENTITY_CREATE, new AtlasEntityHeader(entity)),
                 "create entity: type=", entity.getTypeName());
 
