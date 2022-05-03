6efed030e3ea8fe37b21d88cf09427c6d2ada54d
Nixon Rodrigues <nixon@atlan.com>
2022-05-03 16:12:01 +0530

    Added NPE check, metric log and request in perf log

--- webapp/src/main/java/org/apache/atlas/web/rest/EntityREST.java
+++ webapp/src/main/java/org/apache/atlas/web/rest/EntityREST.java
@@ -1710,7 +1710,7 @@ public class EntityREST {
 
         try {
             if (AtlasPerfTracer.isPerfTraceEnabled(PERF_LOG)) {
-                perf = AtlasPerfTracer.getPerfTracer(PERF_LOG, "EntityREST.repairHasLineage()");
+                perf = AtlasPerfTracer.getPerfTracer(PERF_LOG, "EntityREST.repairHasLineage(" + request + ")");
             }
 
             entitiesStore.repairHasLineage(request);
