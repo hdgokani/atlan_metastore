c433547a143991bc634a3461d489bbcd15f5251c
Nixon Rodrigues <nixon@atlan.com>
2022-06-23 23:52:05 +0530

    META-3044 :NPE fix while creating entity of with struct type attribute

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphMapper.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphMapper.java
@@ -1334,6 +1334,8 @@ public class EntityGraphMapper {
             ret = ctx.getCurrentEdge();
         } else if (ctx.getValue() != null) {
             String edgeLabel = AtlasGraphUtilsV2.getEdgeLabel(ctx.getVertexProperty());
+           // String edgeLabel = ctx.getAttribute().getRelationshipEdgeLabel();
+
 
             AtlasStruct structVal = null;
             if (ctx.getValue() instanceof AtlasStruct) {
@@ -1731,7 +1733,7 @@ public class EntityGraphMapper {
         boolean ret = false;
 
         AtlasEntityType entityType = typeRegistry.getEntityTypeByName(AtlasGraphUtilsV2.getTypeName(ctx.getReferringVertex()));
-        if (entityType.hasRelationshipAttribute(attribute.getName())) {
+        if (entityType !=null && entityType.hasRelationshipAttribute(attribute.getName())) {
             AtlasRelationshipDef relationshipDef = typeRegistry.getRelationshipDefByName(ctx.getAttribute().getRelationshipName());
             ret = !(relationshipDef.getEndDef1().getCardinality() == SET && relationshipDef.getEndDef2().getCardinality() == SET);
         }
