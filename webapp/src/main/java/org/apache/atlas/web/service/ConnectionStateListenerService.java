1ab1f86d35f4d20daaabc4b056096d9b077eb30e
Anshul Mehta <anshul.mk97@gmail.com>
2022-07-12 03:04:02 +0530

    added: connectionStateListener 2

--- webapp/src/main/java/org/apache/atlas/web/service/ConnectionStateListenerService.java
+++ webapp/src/main/java/org/apache/atlas/web/service/ConnectionStateListenerService.java
@@ -5,9 +5,11 @@ import org.apache.curator.framework.state.ConnectionState;
 import org.apache.curator.framework.state.ConnectionStateListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ConnectionStateListenerService implements ConnectionStateListener {
-    private static final Logger LOG = LoggerFactory.getLogger(ActiveInstanceElectorService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ConnectionStateListenerService.class);
 
     @Override
     public void stateChanged(CuratorFramework curatorFramework, ConnectionState connectionState) {
