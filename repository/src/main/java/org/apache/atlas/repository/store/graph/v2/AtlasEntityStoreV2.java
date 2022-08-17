214a2891dc2e1c698d52a7e4bef97ea3fd99d9e0
Nikhil P Bonte <nikhil.bonte@atlan.com>
2022-08-17 16:58:12 +0530

    META-3040 Restrict mutations via entity/relationship APIs

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/AtlasEntityStoreV2.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/AtlasEntityStoreV2.java
@@ -567,7 +567,7 @@ public class AtlasEntityStoreV2 implements AtlasEntityStore {
         AtlasVertex vertex = AtlasGraphUtilsV2.findByGuid(graph, guid);
         ensureNonAccessControlEntityType(Collections.singletonList(getTypeName(vertex)));
 
-        deleteById(vertex);
+        return deleteById(vertex);
     }
 
     @GraphTransaction
