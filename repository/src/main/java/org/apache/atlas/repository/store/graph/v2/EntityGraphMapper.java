18fe856ef14d7cd0358e5eba4949a5596bfd986e
Burak Altas <burak.altas@gmail.com>
2022-08-18 12:30:46 +0300

    META-3091 rollback retry logic on delete classification vertex

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphMapper.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphMapper.java
@@ -3319,18 +3319,7 @@ public class EntityGraphMapper {
             }
             while (offset < propagatedEdgesSize);
 
-            for (int i = 1; i <= MAX_NUMBER_OF_RETRIES; i++) {
-                try {
-                    deleteDelegate.getHandler().deleteClassificationVertex(classificationVertex, true);
-                    break; //do not retry on success
-                } catch (Exception ex) {
-                    if (i == MAX_NUMBER_OF_RETRIES) {
-                        LOG.error(String.format("Maximum retries reached for deleting classification vertex with id %s from graph. Retried %s times. Skipping...", classificationVertex.getId(), i));
-                        continue;
-                    }
-                    LOG.warn(String.format("Vertex with id %s could not be fetched from graph. Retrying for %s time. Exception: %s", classificationVertex.getId(), i, ex.getMessage()));
-                }
-            }
+            deleteDelegate.getHandler().deleteClassificationVertex(classificationVertex, true);
 
             return deletedPropagationsGuid;
         } catch (Exception e) {
