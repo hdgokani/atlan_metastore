a58d7eb17660a9c6ebd8af0be1d0017d02e4e860
suraj5077 <suraj.gupta@atlan.com>
2022-12-28 15:59:24 +0530

    added ES async I/O

--- graphdb/janus/src/main/java/org/apache/atlas/repository/graphdb/janus/AtlasJanusVertexIndexRepository.java
+++ graphdb/janus/src/main/java/org/apache/atlas/repository/graphdb/janus/AtlasJanusVertexIndexRepository.java
@@ -1,16 +1,26 @@
 package org.apache.atlas.repository.graphdb.janus;
 
 import org.apache.atlas.exception.AtlasBaseException;
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.Response;
+import org.elasticsearch.client.ResponseListener;
 
 public interface AtlasJanusVertexIndexRepository {
 
     BulkResponse updateDocsInBulk(BulkRequest bulkRequest) throws AtlasBaseException;
+
     UpdateResponse updateDoc(UpdateRequest request, RequestOptions options) throws AtlasBaseException;
+
+    void updateDocAsync(UpdateRequest request, RequestOptions options, ActionListener<UpdateResponse> listener);
+
+    void updateDocsInBulkAsync(BulkRequest bulkRequest, ActionListener<BulkResponse> listener);
+
     Response performRawRequest(String query, String docId) throws AtlasBaseException;
+
+    void performRawRequestAsync(String query, String docId, ResponseListener listener);
 }
--- graphdb/janus/src/main/java/org/apache/atlas/repository/graphdb/janus/AtlasNestedRelationshipsESQueryBuilder.java
+++ graphdb/janus/src/main/java/org/apache/atlas/repository/graphdb/janus/AtlasNestedRelationshipsESQueryBuilder.java
@@ -2,7 +2,6 @@ package org.apache.atlas.repository.graphdb.janus;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
-import org.apache.atlas.model.instance.AtlasRelationship;
 import org.elasticsearch.action.support.WriteRequest;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.script.Script;
@@ -10,7 +9,6 @@ import org.elasticsearch.script.ScriptType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.*;
 
 import static org.apache.atlas.repository.Constants.INDEX_PREFIX;
@@ -24,7 +22,7 @@ public class AtlasNestedRelationshipsESQueryBuilder {
     private static final String BULK_REQUEST_TIMEOUT_MINUTES = "2m";
     private static final Gson gson = new Gson();
 
-    public static UpdateRequest getQueryForAppendingNestedRelationships(String docId, Map<String, Object> paramsMap) throws IOException {
+    public static UpdateRequest getQueryForAppendingNestedRelationships(String docId, Map<String, Object> paramsMap) {
         return new UpdateRequest().index(INDEX_NAME)
                 .id(docId)
                 .retryOnConflict(RETRY_ON_CONFLICT)
@@ -46,7 +44,7 @@ public class AtlasNestedRelationshipsESQueryBuilder {
                         paramsMap));
     }
 
-    public static UpdateRequest getRelationshipDeletionQuery(AtlasRelationship relationship, String docId, Map<String, Object> params) {
+    public static UpdateRequest getRelationshipDeletionQuery(String docId, Map<String, Object> params) {
         return new UpdateRequest().index(INDEX_NAME).setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE).id(docId)
                 .script(new Script(
                         ScriptType.INLINE, "painless",
@@ -72,7 +70,7 @@ public class AtlasNestedRelationshipsESQueryBuilder {
     }
 
     private static class UpdateESDocWithScriptRequest {
-        private Map<String, Object> script;
+        private final Map<String, Object> script;
         private UpdateESDocWithScriptRequest(Map<String, Object> script) {
             this.script = script;
         }
