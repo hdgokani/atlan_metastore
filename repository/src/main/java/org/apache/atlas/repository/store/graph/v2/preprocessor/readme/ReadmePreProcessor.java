5b713407e9fd5267b4389fc7bea1f93df6b3ae9e
hitarthtalavia <hitarth.talavia@atlan.com>
2023-02-28 18:26:51 +0530

    uniqueAttributes sent instead of asset's guid | requestContext update

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/preprocessor/readme/ReadmePreProcessor.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/preprocessor/readme/ReadmePreProcessor.java
@@ -1,14 +1,13 @@
 package org.apache.atlas.repository.store.graph.v2.preprocessor.readme;
 
+import org.apache.atlas.GraphTransactionInterceptor;
 import org.apache.atlas.RequestContext;
 import org.apache.atlas.exception.AtlasBaseException;
-import org.apache.atlas.model.instance.AtlasEntity;
-import org.apache.atlas.model.instance.AtlasObjectId;
-import org.apache.atlas.model.instance.AtlasStruct;
-import org.apache.atlas.model.instance.EntityMutations;
+import org.apache.atlas.model.instance.*;
 import org.apache.atlas.repository.graph.GraphHelper;
 import org.apache.atlas.repository.graphdb.AtlasGraph;
 import org.apache.atlas.repository.graphdb.AtlasVertex;
+import org.apache.atlas.repository.store.graph.v1.RestoreHandlerV1;
 import org.apache.atlas.repository.store.graph.v2.AtlasGraphUtilsV2;
 import org.apache.atlas.repository.store.graph.v2.EntityGraphRetriever;
 import org.apache.atlas.repository.store.graph.v2.EntityMutationContext;
@@ -20,7 +19,10 @@ import org.apache.atlas.utils.AtlasPerfMetrics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.apache.atlas.model.instance.AtlasEntity.Status.ACTIVE;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 import static org.apache.atlas.model.instance.AtlasEntity.Status.DELETED;
 import static org.apache.atlas.repository.Constants.QUALIFIED_NAME;
 import static org.apache.atlas.repository.Constants.STATE_PROPERTY_KEY;
@@ -28,9 +30,9 @@ import static org.apache.atlas.repository.Constants.STATE_PROPERTY_KEY;
 public class ReadmePreProcessor implements PreProcessor {
     private static final Logger LOG = LoggerFactory.getLogger(GlossaryPreProcessor.class);
 
-    private final AtlasTypeRegistry typeRegistry;
-    private final EntityGraphRetriever entityRetriever;
-    private final AtlasGraph graph;
+    private static AtlasTypeRegistry typeRegistry;
+    private EntityGraphRetriever entityRetriever;
+    private static AtlasGraph graph;
 
     public ReadmePreProcessor(AtlasTypeRegistry typeRegistry, EntityGraphRetriever entityRetriever, AtlasGraph graph) {
         this.entityRetriever = entityRetriever;
@@ -58,39 +60,48 @@ public class ReadmePreProcessor implements PreProcessor {
     }
 
     private void processCreateReadme(AtlasEntity entity, EntityMutationContext context) throws AtlasBaseException {
-        AtlasPerfMetrics.MetricRecorder metricRecorder = RequestContext.get().startMetricRecord("processCreateReadme");
+        RequestContext requestContext = RequestContext.get();
+        AtlasPerfMetrics.MetricRecorder metricRecorder = requestContext.startMetricRecord("processCreateReadme");
         String entityQualifiedName = createQualifiedName(entity);
 
         entity.setAttribute(QUALIFIED_NAME, entityQualifiedName);
 
         AtlasEntityType entityType = typeRegistry.getEntityTypeByName(entity.getTypeName());
-        AtlasVertex vertex = AtlasGraphUtilsV2.findByUniqueAttributes(this.graph, entityType, entity.getAttributes());
+        AtlasVertex vertex = AtlasGraphUtilsV2.findByUniqueAttributes(graph, entityType, entity.getAttributes());
         if(vertex != null && !vertex.getProperty(STATE_PROPERTY_KEY, String.class).equals(DELETED.name())){
             String guidOfExistingReadme = GraphHelper.getGuid(vertex);
             entity.setGuid(guidOfExistingReadme);
             context.cacheEntity(guidOfExistingReadme, vertex, entityType);
         }
         else if(vertex != null){
-            vertex.setProperty(STATE_PROPERTY_KEY,ACTIVE.name());
+            RestoreHandlerV1 restoreHandlerV1 = new RestoreHandlerV1(graph, typeRegistry);
+            GraphTransactionInterceptor.addToVertexStateCache(vertex.getId(), AtlasEntity.Status.ACTIVE);
+            restoreHandlerV1.restoreEntities(Collections.singletonList(vertex));
             context.cacheEntity(entity.getGuid(), vertex, entityType);
         }
-
-        RequestContext.get().endMetricRecord(metricRecorder);
+        AtlasEntityHeader header = new AtlasEntityHeader(entity);
+        requestContext.recordEntityUpdate(header);
+        requestContext.cacheDifferentialEntity(entity);
+        requestContext.endMetricRecord(metricRecorder);
     }
-    private void processUpdateReadme(AtlasStruct entity, AtlasVertex vertex) {
-
-        AtlasPerfMetrics.MetricRecorder metricRecorder = RequestContext.get().startMetricRecord("processUpdateReadme");
+    private void processUpdateReadme(AtlasEntity entity, AtlasVertex vertex) {
+        RequestContext requestContext = RequestContext.get();
+        AtlasPerfMetrics.MetricRecorder metricRecorder = requestContext.startMetricRecord("processUpdateReadme");
         String vertexQnName = vertex.getProperty(QUALIFIED_NAME, String.class);
-        if(vertex.getProperty(STATE_PROPERTY_KEY, String.class).equals(DELETED.name())){
-            vertex.setProperty(STATE_PROPERTY_KEY,ACTIVE.name());
-        }
 
         entity.setAttribute(QUALIFIED_NAME, vertexQnName);
-        RequestContext.get().endMetricRecord(metricRecorder);
+        AtlasEntityHeader header = new AtlasEntityHeader(entity);
+        requestContext.recordEntityUpdate(header);
+        requestContext.cacheDifferentialEntity(entity);
+        requestContext.endMetricRecord(metricRecorder);
     }
 
-    public static String createQualifiedName(AtlasEntity entity) {
+    public static String createQualifiedName(AtlasEntity entity) throws AtlasBaseException {
         AtlasObjectId asset = (AtlasObjectId) entity.getRelationshipAttribute("asset");
-        return asset.getGuid() + "/readme" ;
+        String guid = asset.getGuid();
+        if(guid == null)
+            guid = AtlasGraphUtilsV2.getGuidByUniqueAttributes(graph, typeRegistry.getEntityTypeByName(asset.getTypeName()), asset.getUniqueAttributes());
+
+        return guid + "/readme";
     }
 }
