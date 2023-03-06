28400fb14f1f10f06f4ccf03b1438620f1a4ad6f
hitarthtalavia <hitarth.talavia@atlan.com>
2023-03-06 16:58:31 +0530

    Code Clean

--- repository/src/main/java/org/apache/atlas/repository/store/graph/AtlasTypeDefGraphStore.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/AtlasTypeDefGraphStore.java
@@ -483,7 +483,7 @@ public abstract class AtlasTypeDefGraphStore implements AtlasTypeDefStore {
 
     @Override
     public boolean validateTypeName(AtlasBaseTypeDef typeDef){
-        return Arrays.stream(ATLAS_BUILTIN_TYPES).anyMatch(a -> a.equals(typeDef.getName()));
+        return Arrays.stream(ATLAS_BUILTIN_TYPES).anyMatch(builtinInName -> builtinInName.equals(typeDef.getName()));
     }
 
     @Override
