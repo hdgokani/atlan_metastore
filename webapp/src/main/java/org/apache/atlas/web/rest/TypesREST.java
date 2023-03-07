ab100be46821ae6b5edd7a7a0d511e9ff96d5641
hitarthtalavia <121708100+hitarthtalavia@users.noreply.github.com>
2023-03-07 16:19:58 +0530

    Revert "Meta4194: Added a query param to create a classificationDef of same Display Name"

--- webapp/src/main/java/org/apache/atlas/web/rest/TypesREST.java
+++ webapp/src/main/java/org/apache/atlas/web/rest/TypesREST.java
@@ -422,7 +422,6 @@ public class TypesREST {
      * Bulk create APIs for all atlas type definitions, only new definitions will be created.
      * Any changes to the existing definitions will be discarded
      * @param typesDef A composite wrapper object with corresponding lists of the type definition
-     * @param allowDuplicateDisplayName
      * @return A composite wrapper object with lists of type definitions that were successfully
      * created
      * @throws Exception
@@ -432,7 +431,7 @@ public class TypesREST {
     @POST
     @Path("/typedefs")
     @Timed
-    public AtlasTypesDef createAtlasTypeDefs(final AtlasTypesDef typesDef, @QueryParam("allowDuplicateDisplayName") @DefaultValue("false") boolean allowDuplicateDisplayName) throws AtlasBaseException {
+    public AtlasTypesDef createAtlasTypeDefs(final AtlasTypesDef typesDef) throws AtlasBaseException {
         AtlasPerfTracer perf = null;
         InterProcessMutex lock = null;
         RequestContext.get().setTraceId(UUID.randomUUID().toString());
@@ -443,7 +442,6 @@ public class TypesREST {
                                                                AtlasTypeUtil.toDebugString(typesDef) + ")");
             }
             lock = attemptAcquiringLock();
-            RequestContext.get().setAllowDuplicateDisplayName(allowDuplicateDisplayName);
             typesDef.getBusinessMetadataDefs().forEach(AtlasBusinessMetadataDef::setRandomNameForEntityAndAttributeDefs);
             typesDef.getClassificationDefs().forEach(AtlasClassificationDef::setRandomNameForEntityAndAttributeDefs);
             validateTypeCreateOrUpdate(typesDef);
@@ -485,7 +483,6 @@ public class TypesREST {
     /**
      * Bulk update API for all types, changes detected in the type definitions would be persisted
      * @param typesDef A composite object that captures all type definition changes
-     * @param allowDuplicateDisplayName
      * @return A composite object with lists of type definitions that were updated
      * @throws Exception
      * @HTTP 200 On successful update of requested type definitions
@@ -495,8 +492,7 @@ public class TypesREST {
     @Path("/typedefs")
     @Experimental
     @Timed
-    public AtlasTypesDef updateAtlasTypeDefs(final AtlasTypesDef typesDef, @QueryParam("patch") final boolean patch,
-                                             @QueryParam("allowDuplicateDisplayName") @DefaultValue("false") boolean allowDuplicateDisplayName) throws AtlasBaseException {
+    public AtlasTypesDef updateAtlasTypeDefs(final AtlasTypesDef typesDef, @QueryParam("patch") final boolean patch) throws AtlasBaseException {
         AtlasPerfTracer perf = null;
         InterProcessMutex lock = null;
         RequestContext.get().setTraceId(UUID.randomUUID().toString());
@@ -529,7 +525,6 @@ public class TypesREST {
                 classificationDef.setRandomNameForNewAttributeDefs(existingClassificationDef);
             }
             RequestContext.get().setInTypePatching(patch);
-            RequestContext.get().setAllowDuplicateDisplayName(allowDuplicateDisplayName);
             LOG.info("TypesRest.updateAtlasTypeDefs:: Typedef patch enabled:" + patch);
             validateTypeCreateOrUpdate(typesDef);
             AtlasTypesDef atlasTypesDef = typeDefStore.updateTypesDef(typesDef);
