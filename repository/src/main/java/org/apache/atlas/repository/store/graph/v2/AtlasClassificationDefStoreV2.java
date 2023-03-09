912e07b8b7e4ed8cbeb20b4652fa4a96308eef12
hitarthtalavia <121708100+hitarthtalavia@users.noreply.github.com>
2023-03-09 10:52:22 +0530

    Revert "fix: classification type property skipDisplayNameUniquenessCheck isn't stored in graph store"

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/AtlasClassificationDefStoreV2.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/AtlasClassificationDefStoreV2.java
@@ -80,14 +80,13 @@ class AtlasClassificationDefStoreV2 extends AtlasAbstractDefStoreV2<AtlasClassif
         if (ret != null) {
             throw new AtlasBaseException(AtlasErrorCode.TYPE_ALREADY_EXISTS, classificationDef.getName());
         }
-        if(!classificationDef.getAllowDuplicateDisplayName()) {
-            ret = typeDefStore.findTypeVertexByDisplayName(
-                    classificationDef.getDisplayName(), TypeCategory.TRAIT);
-            if (ret != null) {
-                throw new AtlasBaseException(AtlasErrorCode.TYPE_WITH_DISPLAY_NAME_ALREADY_EXISTS, classificationDef.getDisplayName());
-            }
+        ret = typeDefStore.findTypeVertexByDisplayName(
+                classificationDef.getDisplayName(), TypeCategory.TRAIT);
+        if (ret != null && !classificationDef.getSkipDisplayNameUniquenessCheck()) {
+            throw new AtlasBaseException(AtlasErrorCode.TYPE_WITH_DISPLAY_NAME_ALREADY_EXISTS, classificationDef.getDisplayName());
         }
-            ret = typeDefStore.createTypeVertex(classificationDef);
+
+        ret = typeDefStore.createTypeVertex(classificationDef);
 
         updateVertexPreCreate(classificationDef, (AtlasClassificationType)type, ret);
 
@@ -109,7 +108,7 @@ class AtlasClassificationDefStoreV2 extends AtlasAbstractDefStoreV2<AtlasClassif
         updateVertexAddReferences(classificationDef, vertex);
 
         AtlasClassificationDef ret = toClassificationDef(vertex);
-
+        ret.setSkipDisplayNameUniquenessCheck(classificationDef.getSkipDisplayNameUniquenessCheck());
         if (LOG.isDebugEnabled()) {
             LOG.debug("<== AtlasClassificationDefStoreV1.create({}, {}): {}", classificationDef, preCreateResult, ret);
         }
@@ -190,12 +189,13 @@ class AtlasClassificationDefStoreV2 extends AtlasAbstractDefStoreV2<AtlasClassif
             throw new AtlasBaseException(AtlasErrorCode.MISSING_CLASSIFICATION_DISPLAY_NAME);
         }
 
-        if(!classifiDef.getAllowDuplicateDisplayName()){
-            AtlasVertex ret = typeDefStore.findTypeVertexByDisplayName(
-                    classifiDef.getDisplayName(), DataTypes.TypeCategory.TRAIT);
-            if (ret != null && (classifiDef.getGuid() == null || !classifiDef.getGuid().equals(ret.getProperty(Constants.GUID_PROPERTY_KEY, String.class)))){
-                throw new AtlasBaseException(AtlasErrorCode.TYPE_WITH_DISPLAY_NAME_ALREADY_EXISTS, classifiDef.getDisplayName());
-            }
+        //validate uniqueness of display name
+        AtlasVertex ret = typeDefStore.findTypeVertexByDisplayName(
+                classifiDef.getDisplayName(), DataTypes.TypeCategory.TRAIT);
+        if (ret != null && (
+                classifiDef.getGuid() == null || !classifiDef.getGuid().equals(ret.getProperty(Constants.GUID_PROPERTY_KEY, String.class)))
+                 && (!classifiDef.getSkipDisplayNameUniquenessCheck())) {
+            throw new AtlasBaseException(AtlasErrorCode.TYPE_WITH_DISPLAY_NAME_ALREADY_EXISTS, classifiDef.getDisplayName());
         }
     }
 
@@ -213,6 +213,7 @@ class AtlasClassificationDefStoreV2 extends AtlasAbstractDefStoreV2<AtlasClassif
         AtlasClassificationDef ret = StringUtils.isNotBlank(classifiDef.getGuid())
                   ? updateByGuid(classifiDef.getGuid(), classifiDef) : updateByName(classifiDef.getName(), classifiDef);
 
+        ret.setSkipDisplayNameUniquenessCheck(classifiDef.getSkipDisplayNameUniquenessCheck());
         if (LOG.isDebugEnabled()) {
             LOG.debug("<== AtlasClassificationDefStoreV1.update({}): {}", classifiDef, ret);
         }
@@ -391,7 +392,6 @@ class AtlasClassificationDefStoreV2 extends AtlasAbstractDefStoreV2<AtlasClassif
 
             ret.setSuperTypes(typeDefStore.getSuperTypeNames(vertex));
             ret.setEntityTypes(typeDefStore.getEntityTypeNames(vertex));
-            ret.setAllowDuplicateDisplayName(typeDefStore.getAllowDuplicateDisplayNameProperty(vertex));
         }
 
         return ret;
