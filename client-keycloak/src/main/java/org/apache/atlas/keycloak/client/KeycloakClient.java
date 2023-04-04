6d82a8521a3a49c462eff51f8c246414c1fbd166
nikhilbonte21 <nikhil.bonte@atlan.com>
2023-04-04 16:08:53 +0530

    META-4203 Fix Keycloak issues

--- client-keycloak/src/main/java/org/apache/atlas/keycloak/client/KeycloakClient.java
+++ client-keycloak/src/main/java/org/apache/atlas/keycloak/client/KeycloakClient.java
@@ -137,16 +137,21 @@ public class KeycloakClient {
 
     public List<UserRepresentation> getAllUsers() {
         int start = 0;
-        int size = 100;
+        int size = 500;
+        boolean found = true;
 
         List<UserRepresentation> ret = new ArrayList<>();
 
         do {
             List<UserRepresentation> userRepresentations = getRealm().users().list(start, size);
-            ret.addAll(userRepresentations);
-            start += size;
+            if (CollectionUtils.isNotEmpty(userRepresentations)) {
+                ret.addAll(userRepresentations);
+                start += size;
+            } else {
+                found = false;
+            }
 
-        } while (CollectionUtils.isNotEmpty(ret) && ret.size() % size == 0);
+        } while (found && ret.size() % size == 0);
 
         return ret;
     }
@@ -154,32 +159,42 @@ public class KeycloakClient {
     public List<GroupRepresentation> getAllGroups() {
 
         int start = 0;
-        int size = 100;
+        int size = 500;
+        boolean found = true;
 
         List<GroupRepresentation> ret = new ArrayList<>();
 
         do {
             List<GroupRepresentation> groupRepresentations = getRealm().groups().groups(start, size);
-            ret.addAll(groupRepresentations);
-            start += size;
+            if (CollectionUtils.isNotEmpty(groupRepresentations)) {
+                ret.addAll(groupRepresentations);
+                start += size;
+            } else {
+                found = false;
+            }
 
-        } while (CollectionUtils.isNotEmpty(ret) && ret.size() % size == 0);
+        } while (found && ret.size() % size == 0);
 
         return ret;
     }
 
     public List<RoleRepresentation> getAllRoles() {
         int start = 0;
-        int size = 100;
+        int size = 500;
+        boolean found = true;
 
         List<RoleRepresentation> ret = new ArrayList<>();
 
         do {
             List<RoleRepresentation> roleRepresentations = getRealm().roles().list(start, size);
-            ret.addAll(roleRepresentations);
-            start += size;
+            if (CollectionUtils.isNotEmpty(roleRepresentations)) {
+                ret.addAll(roleRepresentations);
+                start += size;
+            } else {
+                found = false;
+            }
 
-        } while (CollectionUtils.isNotEmpty(ret) && ret.size() % size == 0);
+        } while (found && ret.size() % size == 0);
 
         return ret;
     }
