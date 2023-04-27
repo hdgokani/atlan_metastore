2d9c55134535dbb787998ac8d8ff56b73eb1a8eb
Suman Das <59254445+sumandas0@users.noreply.github.com>
2023-04-27 10:38:23 +0530

    feat: Add type fetch function

--- webapp/src/main/java/org/apache/atlas/web/rest/MigrationREST.java
+++ webapp/src/main/java/org/apache/atlas/web/rest/MigrationREST.java
@@ -3,15 +3,24 @@ package org.apache.atlas.web.rest;
 import javax.ws.rs.Path;
 import org.apache.atlas.RequestContext;
 import org.apache.atlas.annotation.Timed;
+import org.apache.atlas.discovery.EntityDiscoveryService;
 import org.apache.atlas.exception.AtlasBaseException;
+import org.apache.atlas.model.discovery.IndexSearchParams;
+import org.apache.atlas.model.discovery.SearchParams;
 import org.apache.atlas.model.instance.AtlasEntity;
+import org.apache.atlas.model.instance.AtlasEntityHeader;
 import org.apache.atlas.model.instance.EntityMutationResponse;
+import org.apache.atlas.repository.graphdb.AtlasGraph;
+import org.apache.atlas.repository.graphdb.AtlasIndexQuery;
+import org.apache.atlas.repository.graphdb.AtlasVertex;
+import org.apache.atlas.repository.graphdb.DirectIndexQueryResult;
 import org.apache.atlas.repository.store.graph.AtlasEntityStore;
 import org.apache.atlas.repository.store.graph.v2.AtlasEntityStream;
 import org.apache.atlas.repository.store.graph.v2.EntityStream;
 import org.apache.atlas.repository.store.users.KeycloakStore;
 import org.apache.atlas.transformer.PreProcessorPoliciesTransformer;
 import org.apache.atlas.utils.AtlasPerfTracer;
+import org.apache.atlas.v1.model.instance.Id;
 import org.apache.atlas.web.util.Servlets;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
@@ -24,12 +33,12 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.apache.atlas.authorize.AtlasAuthorizerFactory.ATLAS_AUTHORIZER_IMPL;
 import static org.apache.atlas.authorize.AtlasAuthorizerFactory.CURRENT_AUTHORIZER_IMPL;
 import static org.apache.atlas.repository.Constants.*;
+import static org.apache.atlas.repository.util.AccessControlUtils.ATTR_POLICY_SERVICE_NAME;
 
 @Path("migration")
 @Singleton
@@ -47,6 +56,8 @@ public class MigrationREST {
     private final AtlasEntityStore entityStore;
     private final PreProcessorPoliciesTransformer transformer;
     private KeycloakStore keycloakStore;
+    private AtlasGraph graph;
+    private EntityDiscoveryService discoveryService;
 
     @Inject
     public MigrationREST(AtlasEntityStore entityStore) {
@@ -144,6 +155,87 @@ public class MigrationREST {
 
     }
 
+    @POST
+    @Path("search/{typeName}")
+    @Timed
+    public List<AtlasEntity.AtlasEntityWithExtInfo> searchForType(@PathParam("typeName") String typeName, @QueryParam("minExtInfo") @DefaultValue("false") boolean minExtInfo, @QueryParam("ignoreRelationships") @DefaultValue("false") boolean ignoreRelationships) throws Exception {
+        AtlasPerfTracer perf = null;
+        try {
+            if (AtlasPerfTracer.isPerfTraceEnabled(PERF_LOG)) {
+                perf = AtlasPerfTracer.getPerfTracer(PERF_LOG, "MigrationREST.searchUsingDslQuery(" + typeName + ")");
+            }
+
+            List<AtlasEntity.AtlasEntityWithExtInfo> ret = new ArrayList<>();
+            SearchParams params = new SearchParams();
+            params.setAttributes(new HashSet<>(Arrays.asList("roleId", "description")));
+            IndexSearchParams indexSearchParams = (IndexSearchParams) params;
+
+            Map<String, Object> dsl = getMap("size", 0);
+
+            List<Map<String, Object>> mustClauseList = new ArrayList<>();
+            mustClauseList.add(getMap("term", getMap("__typeName.keyword", typeName)));
+            mustClauseList.add(getMap("match", getMap("__state", Id.EntityState.ACTIVE)));
+
+            dsl.put("query", getMap("bool", getMap("must", mustClauseList)));
+
+            dsl.put("sort", Collections.singleton(getMap("__guid", getMap("order", "desc"))));
+
+            indexSearchParams.setDsl(dsl);
+
+            int from = 0;
+            int size = 100;
+            boolean found = true;
+
+            do {
+                dsl.put("from", from);
+                dsl.put("size", size);
+                indexSearchParams.setDsl(dsl);
+                List<AtlasEntity.AtlasEntityWithExtInfo> entities = getEntitiesByIndexSearch(indexSearchParams, minExtInfo, ignoreRelationships);
+
+                if (entities != null) {
+                    ret.addAll(entities);
+                } else {
+                    found = false;
+                }
+                from += size;
+
+            } while (found && ret.size() % size == 0);
+
+            return ret;
+
+        } finally {
+            AtlasPerfTracer.log(perf);
+        }
+    }
+
+    private List<AtlasEntity.AtlasEntityWithExtInfo> getEntitiesByIndexSearch(IndexSearchParams indexSearchParams, Boolean minExtInfo, boolean ignoreRelationships) throws AtlasBaseException{
+        List<AtlasEntity.AtlasEntityWithExtInfo> ret = new ArrayList<>();
+        AtlasIndexQuery indexQuery = null;
+        String indexName = "janusgraph_vertex_index";
+        indexQuery = graph.elasticsearchQuery(indexName);
+        DirectIndexQueryResult indexQueryResult = indexQuery.vertices(indexSearchParams);
+        Iterator<AtlasIndexQuery.Result> iterator = indexQueryResult.getIterator();
+        while (iterator.hasNext()) {
+            AtlasIndexQuery.Result result = iterator.next();
+            AtlasVertex vertex = result.getVertex();
+            if (vertex == null) {
+                LOG.warn("vertex is null");
+                continue;
+            }
+            AtlasEntity.AtlasEntityWithExtInfo entityWithExtInfo = entityStore.getById(vertex.getProperty("__guid", String.class), minExtInfo, ignoreRelationships);
+            if (entityWithExtInfo != null) {
+                ret.add(entityWithExtInfo);
+            }
+
+        }
+        return ret;
+    }
+
+    private Map<String, Object> getMap(String key, Object value) {
+        Map<String, Object> map = new HashMap<>();
+        map.put(key, value);
+        return map;
+    }
 
     private RoleRepresentation createCollectionAdminRole(AtlasEntity collection) throws AtlasBaseException {
         //create Admin role
