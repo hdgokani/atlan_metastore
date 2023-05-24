6794cc7e498b91235417a35d5d9a1b453adead69
Suman Das <59254445+sumandas0@users.noreply.github.com>
2023-05-24 13:22:43 +0530

    feat: Added feature to get only accessible assets

--- intg/src/main/java/org/apache/atlas/model/discovery/IndexSearchParams.java
+++ intg/src/main/java/org/apache/atlas/model/discovery/IndexSearchParams.java
@@ -42,6 +42,10 @@ public class IndexSearchParams extends SearchParams {
         queryString = AtlasType.toJson(dsl);
     }
 
+    public Map getDsl() {
+        return dsl;
+    }
+
     public boolean isAllowDeletedRelations() {
         return allowDeletedRelations;
     }
