d68da215b0451bf53b242fba47936c1ecb95aef3
Nikhil P Bonte <nikhil.bonte@atlan.com>
2023-08-08 20:20:22 +0530

    Resolve duplicate filed name issue

--- common/src/main/java/org/apache/atlas/repository/Constants.java
+++ common/src/main/java/org/apache/atlas/repository/Constants.java
@@ -140,7 +140,6 @@ public final class Constants {
      */
     public static final String CONNECTION_ENTITY_TYPE       = "Connection";
     public static final String QUERY_ENTITY_TYPE            = "Query";
-    public static final String README_ENTITY_TYPE           = "Readme";
     public static final String QUERY_FOLDER_ENTITY_TYPE     = "Folder";
     public static final String COLLECTION_ENTITY_TYPE       = "Collection";
 
