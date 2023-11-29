fe7009abfbf93f8742f5e3873e81c1354fbf8a98
ektavarma10 <ekta.varma@atlan.com>
2023-11-30 02:41:34 +0530

    Revert "add parllelisation for checkAccessAndScrub method"
    This reverts commit 1e226a9e

--- auth-plugin-atlas/src/main/java/org/apache/atlas/authorization/atlas/authorizer/RangerAtlasAuthorizer.java
+++ auth-plugin-atlas/src/main/java/org/apache/atlas/authorization/atlas/authorizer/RangerAtlasAuthorizer.java
@@ -61,14 +61,6 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
 import java.util.Set;
-import java.util.Objects;
-import java.util.ArrayList;
-import java.util.Optional;
-
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
 
 import static org.apache.atlas.authorization.atlas.authorizer.RangerAtlasAuthorizerUtil.*;
 import static org.apache.atlas.authorize.AtlasAuthorizationUtils.getCurrentUserGroups;
@@ -88,8 +80,6 @@ public class RangerAtlasAuthorizer implements AtlasAuthorizer {
         add(AtlasPrivilege.ENTITY_REMOVE_CLASSIFICATION);
         add(AtlasPrivilege.ENTITY_UPDATE_CLASSIFICATION);
     }};
-
-    static final ExecutorService entityAccessThreadpool = Executors.newFixedThreadPool(10);
 
     @Override
     public void init() {
@@ -599,27 +589,22 @@ public class RangerAtlasAuthorizer implements AtlasAuthorizer {
             if (RangerPerfTracer.isPerfTraceEnabled(PERF_LOG))
                 perf = RangerPerfTracer.getPerfTracer(PERF_LOG, "RangerAtlasAuthorizer.scrubSearchResults(" + request + ")");
             AtlasSearchResult result = request.getSearchResult();
-            List<AtlasEntityHeader> entitiesToCheck = new ArrayList<>();
-
             if (CollectionUtils.isNotEmpty(result.getEntities())) {
-                entitiesToCheck.addAll(result.getEntities());
+                for (AtlasEntityHeader entity : result.getEntities()) {
+                    checkAccessAndScrub(entity, request, isScrubAuditEnabled);
+                }
             }
-
             if (CollectionUtils.isNotEmpty(result.getFullTextResult())) {
-                entitiesToCheck.addAll(
-                        result.getFullTextResult()
-                                .stream()
-                                .filter(Objects::nonNull)
-                                .map(res -> res.getEntity())
-                                .collect(Collectors.toList())
-                );
+                for (AtlasSearchResult.AtlasFullTextResult fullTextResult : result.getFullTextResult()) {
+                    if (fullTextResult != null)
+                        checkAccessAndScrub(fullTextResult.getEntity(), request, isScrubAuditEnabled);
+                }
             }
-
             if (MapUtils.isNotEmpty(result.getReferredEntities())) {
-                entitiesToCheck.addAll(result.getReferredEntities().values());
+                for (AtlasEntityHeader entity : result.getReferredEntities().values()) {
+                    checkAccessAndScrub(entity, request, isScrubAuditEnabled);
+                }
             }
-
-            checkAccessAndScrubAsync(entitiesToCheck, request, isScrubAuditEnabled);
         } finally {
             RangerPerfTracer.log(perf);
         }
@@ -639,38 +624,6 @@ public class RangerAtlasAuthorizer implements AtlasAuthorizer {
         filterTypes(request, typesDef.getRelationshipDefs());
         filterTypes(request, typesDef.getBusinessMetadataDefs());
 
-    }
-
-    private void checkAccessAndScrubAsync(List<AtlasEntityHeader> entitiesToCheck, AtlasSearchResultScrubRequest request, boolean isScrubAuditEnabled) throws AtlasAuthorizationException {
-        LOG.info("Creating futures to check access and scrub " + entitiesToCheck.size() + " entities");
-        long startTime = System.currentTimeMillis();
-        LOG.info("Started checkAccessAndScrubAsync: " + System.currentTimeMillis());
-        List<CompletableFuture<AtlasAuthorizationException>> completableFutures = entitiesToCheck
-                .stream()
-                .map(entity -> CompletableFuture.supplyAsync(() -> {
-                    try {
-                        checkAccessAndScrub(entity, request, isScrubAuditEnabled);
-                        return null;
-                    } catch (AtlasAuthorizationException e) {
-                        return e;
-                    }
-                }, entityAccessThreadpool))
-                .collect(Collectors.toList());
-        LOG.info("Completed async submission " + (System.currentTimeMillis()-startTime));
-        // wait for all threads to complete their execution
-        CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[0])).join();
-        LOG.info("Completed waiting for completion of threads: " + (System.currentTimeMillis()-startTime));
-        // get the first exception from any checkAccessAndScrub calls
-        Optional<AtlasAuthorizationException> maybeAuthException = completableFutures
-                .stream()
-                .map(CompletableFuture::join)
-                .filter(Objects::nonNull)
-                .findFirst();
-
-        LOG.info("Async check access and scrub is complete: "+ (System.currentTimeMillis()-startTime));
-        if (maybeAuthException.isPresent()) {
-            throw maybeAuthException.get();
-        }
     }
 
     private void filterTypes(AtlasAccessRequest request, List<? extends AtlasBaseTypeDef> typeDefs)throws AtlasAuthorizationException {
