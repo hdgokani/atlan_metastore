6ba74669775148f0618f21095c5c67e6a6d9ea9f
Suman Das <59254445+sumandas0@users.noreply.github.com>
2024-01-22 17:44:01 +0530

    feat: update dep and objects for shaded jar

--- auth-agents-common/src/main/java/org/apache/atlas/plugin/model/RangerPolicy.java
+++ auth-agents-common/src/main/java/org/apache/atlas/plugin/model/RangerPolicy.java
@@ -19,6 +19,7 @@
 
 package org.apache.atlas.plugin.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.htrace.shaded.fasterxml.jackson.annotation.JsonInclude;
 
@@ -32,6 +33,7 @@ import java.util.List;
 import java.util.Map;
 
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
