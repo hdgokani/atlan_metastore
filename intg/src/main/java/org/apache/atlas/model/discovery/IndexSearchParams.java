c76a1295493727404a286a3acb7fe2bbd7d80c5b
Nikhil P Bonte <nikhil.bonte@atlan.com>
2024-02-13 19:57:23 +0530

    DG-215 Full restrictions on READ indexsearch

--- intg/src/main/java/org/apache/atlas/model/discovery/IndexSearchParams.java
+++ intg/src/main/java/org/apache/atlas/model/discovery/IndexSearchParams.java
@@ -33,6 +33,11 @@ public class IndexSearchParams extends SearchParams {
         return queryString;
     }
 
+    @Override
+    public void setQuery(String query) {
+        this.queryString = query;
+    }
+
     public Map getDsl() {
         return dsl;
     }
--- intg/src/main/java/org/apache/atlas/model/discovery/SearchParams.java
+++ intg/src/main/java/org/apache/atlas/model/discovery/SearchParams.java
@@ -18,6 +18,7 @@ public class SearchParams {
     boolean suppressLogs;
     boolean excludeMeanings;
     boolean excludeClassifications;
+    boolean enableFullRestriction;
 
     RequestMetadata requestMetadata = new RequestMetadata();
     boolean showHighlights;
@@ -26,8 +27,16 @@ public class SearchParams {
         return getQuery();
     }
 
+    public boolean getEnableFullRestriction() {
+        return enableFullRestriction;
+    }
+
     public Set<String> getAttributes() {
         return attributes;
+    }
+
+    public void setQuery(String query) {
+        setQuery(query);
     }
 
     public void setAttributes(Set<String> attributes) {
