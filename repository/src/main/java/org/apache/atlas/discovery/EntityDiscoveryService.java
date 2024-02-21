a41823daeadeacabba6abcdc57654872dc32f52b
Nikhil P Bonte <nikhil.bonte@atlan.com>
2024-02-21 16:42:25 +0530

    Revert "Ignore full restrictions for Persona/Purpose request"
    This reverts commit 031b00cb767da3a4a3b80770d9c4c64638e643df.

--- repository/src/main/java/org/apache/atlas/discovery/EntityDiscoveryService.java
+++ repository/src/main/java/org/apache/atlas/discovery/EntityDiscoveryService.java
@@ -1000,14 +1000,14 @@ public class EntityDiscoveryService implements AtlasDiscoveryService {
             String indexName = getIndexName(params);
 
             indexQuery = graph.elasticsearchQuery(indexName);
-            if (searchParams.getEnableFullRestriction() && VERTEX_INDEX_NAME.equals(indexName)) {
+            if (searchParams.getEnableFullRestriction() && !VERTEX_INDEX_NAME.equals(indexName)) {
                 addPreFiltersToSearchQuery(searchParams);
             }
             //LOG.info(searchParams.getQuery());
             AtlasPerfMetrics.MetricRecorder elasticSearchQueryMetric = RequestContext.get().startMetricRecord("elasticSearchQuery");
             DirectIndexQueryResult indexQueryResult = indexQuery.vertices(searchParams);
             RequestContext.get().endMetricRecord(elasticSearchQueryMetric);
-            prepareSearchResult(ret, indexQueryResult, resultAttributes, true, indexName);
+            prepareSearchResult(ret, indexQueryResult, resultAttributes, true);
 
             ret.setAggregations(indexQueryResult.getAggregationMap());
             ret.setApproximateCount(indexQuery.vertexTotals());
@@ -1044,8 +1044,7 @@ public class EntityDiscoveryService implements AtlasDiscoveryService {
         }
     }
 
-    private void prepareSearchResult(AtlasSearchResult ret, DirectIndexQueryResult indexQueryResult, Set<String> resultAttributes,
-                                     boolean fetchCollapsedResults, String indexName) throws AtlasBaseException {
+    private void prepareSearchResult(AtlasSearchResult ret, DirectIndexQueryResult indexQueryResult, Set<String> resultAttributes, boolean fetchCollapsedResults) throws AtlasBaseException {
         SearchParams searchParams = ret.getSearchParameters();
         try {
             if(LOG.isDebugEnabled()){
@@ -1092,7 +1091,7 @@ public class EntityDiscoveryService implements AtlasDiscoveryService {
 
                         DirectIndexQueryResult indexQueryCollapsedResult = result.getCollapseVertices(collapseKey);
                         collapseRet.setApproximateCount(indexQueryCollapsedResult.getApproximateCount());
-                        prepareSearchResult(collapseRet, indexQueryCollapsedResult, collapseResultAttributes, false, indexName);
+                        prepareSearchResult(collapseRet, indexQueryCollapsedResult, collapseResultAttributes, false);
 
                         collapseRet.setSearchParameters(null);
                         collapse.put(collapseKey, collapseRet);
@@ -1112,7 +1111,7 @@ public class EntityDiscoveryService implements AtlasDiscoveryService {
                 throw e;
         }
 
-        if (!searchParams.getEnableFullRestriction() || !VERTEX_INDEX_NAME.equals(indexName)) {
+        if (!searchParams.getEnableFullRestriction()) {
             scrubSearchResults(ret, searchParams.getSuppressLogs());
         }
     }
