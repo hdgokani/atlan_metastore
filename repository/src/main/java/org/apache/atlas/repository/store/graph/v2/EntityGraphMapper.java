366acac4e7e41652fcc57a66efd6273da4928076
Nikhil P Bonte <nikhil.bonte@atlan.com>
2024-09-03 11:15:28 +0530

    DG-1746 FIX NPE

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphMapper.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphMapper.java
@@ -2276,6 +2276,10 @@ public class EntityGraphMapper {
     }
 
     public static void validateCustomRelationshipAttributeValue(Set<String> restrictedValues, Map<String, Object> attributes) throws AtlasBaseException {
+        if (MapUtils.isEmpty(attributes)) {
+            return;
+        }
+
         for (String key : attributes.keySet()) {
             if (key.equals("toType") || key.equals("fromType")) {
                 String value = (String) attributes.get(key);
