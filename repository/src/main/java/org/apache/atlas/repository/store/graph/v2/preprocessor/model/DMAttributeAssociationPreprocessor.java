3bb6e19b72ef8bda9ca2a006cce8c1268234762d
aarshi <aarshi.arora@atlan.com>
2024-09-26 18:31:22 +0530

    fix name

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/preprocessor/model/DMAttributeAssociationPreprocessor.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/preprocessor/model/DMAttributeAssociationPreprocessor.java
@@ -6,7 +6,6 @@ import org.apache.atlas.exception.AtlasBaseException;
 import org.apache.atlas.model.instance.AtlasEntity;
 import org.apache.atlas.model.instance.AtlasStruct;
 import org.apache.atlas.model.instance.EntityMutations;
-import org.apache.atlas.repository.Constants;
 import org.apache.atlas.repository.graphdb.AtlasVertex;
 import org.apache.atlas.repository.store.graph.AtlasRelationshipStore;
 import org.apache.atlas.repository.store.graph.v2.EntityGraphMapper;
@@ -20,7 +19,6 @@ import org.slf4j.LoggerFactory;
 import java.util.Map;
 
 import static org.apache.atlas.repository.Constants.*;
-import static org.apache.atlas.repository.Constants.ATLAS_DM_ENTITY_ASSOCIATION_TYPE;
 import static org.apache.atlas.repository.store.graph.v2.preprocessor.PreProcessorUtils.isNameInvalid;
 
 public class DMAttributeAssociationPreprocessor extends AbstractModelPreProcessor{
@@ -80,8 +78,8 @@ public class DMAttributeAssociationPreprocessor extends AbstractModelPreProcesso
         long now = RequestContext.get().getRequestTime();
 
         ModelResponse modelResponse = replicateDMAssociation(entity, vertex, now);
-        AtlasEntity copyEntity = modelResponse.getCopyEntity();
-        AtlasVertex copyVertex = modelResponse.getCopyVertex();
+        AtlasEntity copyEntity = modelResponse.getReplicaEntity();
+        AtlasVertex copyVertex = modelResponse.getReplicaVertex();
         applyDiffs(entity, copyEntity, ATLAS_DM_ATTRIBUTE_ASSOCIATION_TYPE);
         unsetExpiredDates(copyEntity, copyVertex);
 
@@ -89,16 +87,16 @@ public class DMAttributeAssociationPreprocessor extends AbstractModelPreProcesso
         // case when a mapping is added
         if (entity.getAppendRelationshipAttributes() != null) {
             Map<String, Object> appendRelationshipAttributes = processRelationshipAttributesForAttribute(entity, entity.getAppendRelationshipAttributes(), context);
-            modelResponse.getCopyEntity().setAppendRelationshipAttributes(appendRelationshipAttributes);
+            modelResponse.getReplicaEntity().setAppendRelationshipAttributes(appendRelationshipAttributes);
             context.removeUpdatedWithRelationshipAttributes(entity);
-            context.setUpdatedWithRelationshipAttributes(modelResponse.getCopyEntity());
+            context.setUpdatedWithRelationshipAttributes(modelResponse.getReplicaEntity());
         }
 
         if (entity.getRemoveRelationshipAttributes() != null) {
             Map<String, Object> appendRelationshipAttributes = processRelationshipAttributesForAttribute(entity, entity.getRemoveRelationshipAttributes(), context);
-            modelResponse.getCopyEntity().setRemoveRelationshipAttributes(appendRelationshipAttributes);
+            modelResponse.getReplicaEntity().setRemoveRelationshipAttributes(appendRelationshipAttributes);
             context.removeUpdatedWithDeleteRelationshipAttributes(entity);
-            context.setUpdatedWithRemoveRelationshipAttributes(modelResponse.getCopyEntity());
+            context.setUpdatedWithRemoveRelationshipAttributes(modelResponse.getReplicaEntity());
         }
     }
 }
