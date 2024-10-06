606f01f0d8b7113686f487427eaad8c26ca0071b
Suman Das <59254445+sumandas0@users.noreply.github.com>
2024-10-06 22:52:17 +0530

    fix: fix typo

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphMapper.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphMapper.java
@@ -3877,7 +3877,7 @@ public class EntityGraphMapper {
                     //Sending audit request for all entities at once
                     entityChangeNotifier.onClassificationsDeletedFromEntities(propagatedEntities, Collections.singletonList(classification));
                 }
-                RequestContext.get().endMetricRecord(removedPropagations);
+                RequestContext.get().endMetricRecord(metricRecorderEntity);
             }
 
             AtlasPerfTracer.log(perf);
