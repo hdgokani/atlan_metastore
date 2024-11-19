8c7ca544b2e93e9832b56db07037c97692920816
aarshi <aarshi.arora@atlan.com>
2024-11-19 08:16:33 +0530

    remove method

--- repository/src/main/java/org/apache/atlas/discovery/EntityDiscoveryService.java
+++ repository/src/main/java/org/apache/atlas/discovery/EntityDiscoveryService.java
@@ -1148,10 +1148,6 @@ public class EntityDiscoveryService implements AtlasDiscoveryService {
 
             ret.addEntity(header);
         });
-
-        if (!searchParams.getEnableFullRestriction()) {
-            scrubSearchResults(ret, searchParams.getSuppressLogs());
-        }
     }
 
     // Non-recursive collapse processing
