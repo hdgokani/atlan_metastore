e403fb62ac3f29318adad62df51ba7c57c39423e
aarshi <aarshi.arora@atlan.com>
2024-12-06 14:38:49 +0530

    push flag

--- intg/src/main/java/org/apache/atlas/AtlasConfiguration.java
+++ intg/src/main/java/org/apache/atlas/AtlasConfiguration.java
@@ -117,8 +117,12 @@ public enum AtlasConfiguration {
     ENABLE_ASYNC_INDEXSEARCH("atlas.indexsearch.async.enable", false),
     ATLAS_INDEXSEARCH_QUERY_SIZE_MAX_LIMIT("atlas.indexsearch.query.size.max.limit", 100000),
     ATLAS_INDEXSEARCH_LIMIT_UTM_TAGS("atlas.indexsearch.limit.ignore.utm.tags", ""),
-    ATLAS_INDEXSEARCH_ENABLE_API_LIMIT("atlas.indexsearch.enable.api.limit", false),
 
+    /**
+     *   hits elastic search async API
+     */
+    ATLAS_INDEXSEARCH_ENABLE_API_LIMIT("atlas.indexsearch.enable.api.limit", false),
+    ATLAS_INDEXSEARCH_ENABLE_JANUS_OPTIMISATION("atlas.indexsearch.enable.janus.optimization", false),
     /***
      * This configuration is used to enable fetching non primitive attributes in index search
      */
