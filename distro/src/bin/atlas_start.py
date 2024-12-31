4f3e03fefce80f4f35b6e816ac049fa3af15aaff
Nikhil P Bonte <nikhil.bonte@atlan.com>
2024-12-31 18:31:19 +0530

    Java17 Fix opens option at startup
    (cherry picked from commit cc3b53a2c2906f06e961660181c9ee04c1314ea9)

--- distro/src/bin/atlas_start.py
+++ distro/src/bin/atlas_start.py
@@ -54,7 +54,7 @@ def main():
     else:
         jvm_opts_list = (ATLAS_LOG_OPTS % (jvm_logdir, "atlas_setup")).split()
 
-    jvm_opts_list.extend(ADD_OPENS_JAVA_17_JVM_OPTS)
+    jvm_opts_list.insert(0, ADD_OPENS_JAVA_17_JVM_OPTS)
 
     cmd_opts = (ATLAS_COMMAND_OPTS % jvm_atlas_home)
     jvm_opts_list.extend(cmd_opts.split())
@@ -169,6 +169,7 @@ def main():
 
 
 def start_atlas_server(atlas_classpath, atlas_pid_file, jvm_logdir, jvm_opts_list, web_app_path):
+    print(f"{jvm_opts_list}\n")
     args = ["-app", web_app_path]
     args.extend(sys.argv[1:])
     process = mc.java("org.apache.atlas.Atlas", args, atlas_classpath, jvm_opts_list, jvm_logdir)
