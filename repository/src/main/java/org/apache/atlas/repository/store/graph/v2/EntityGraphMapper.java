b02cbcc4f9e24305c85a75fbf682daea1ece8602
Abhijeet Kumar <abhijeet.kumar@atlan.com>
2025-01-30 17:25:42 +0530

    fixing dry for task vertex updation

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphMapper.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphMapper.java
@@ -3575,7 +3575,6 @@ public class EntityGraphMapper {
             LOG.error("Error occurred while adding classification propagation for classification with propagation id {}", classificationVertex.getIdForDisplay());
             throw exception;
         } finally {
-            MetricRecorder classificationPropagationMetricRecorder = null;
             RequestContext.get().endMetricRecord(classificationPropagationMetricRecorder);
         }
 
